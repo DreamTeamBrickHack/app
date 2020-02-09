ced4577ac1682d31035c44aca38430aad798f393
Adit-COCO-Garg <adit.garg21k@gmail.com>
2020-02-09 03:39:02 -0500

    fixed UI

--- src/maps.js
+++ src/maps.js
@@ -2,21 +2,17 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapStyles = {
-    width: '100%',
-    height: '100%',
+    width: '72vw',
+    height: '95vh',
   };
-  
+
 export class MapContainer extends Component {
     constructor(props) {
       super(props);
       
       this.state = {
-        stores: [{lat: 47.49855629475769, lng: -122.14184416996333},
-                {latitude: 47.359423, longitude: -122.021071},
-                {latitude: 47.2052192687988, longitude: -121.988426208496},
-                {latitude: 47.6307081, longitude: -122.1434325},
-                {latitude: 47.3084488, longitude: -122.2140121},
-                {latitude: 47.5524695, longitude: -122.0425407}]
+        stores: [
+                {latitude: 30.5928, longitude: 114.3055}]
       }
     }
   
@@ -36,7 +32,8 @@ export class MapContainer extends Component {
             google={this.props.google}
             zoom={8}
             style={mapStyles}
-            initialCenter={{ lat: 47.444, lng: -122.176}}
+            id={"mappyBoi"}
+            initialCenter={{ lat: 30.5928, lng: 114.3055}}
           >
             {this.displayMarkers()}
           </Map>
